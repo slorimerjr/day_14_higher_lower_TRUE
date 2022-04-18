f4028abb29731e9debca0041657adbe7c40d337d
Sam Lorimer <slorimerjr@gmail.com>
2022-04-18 23:57:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+#import data, logo
+import random
+from art import logo
+from art import vs
+from game_data import data
+# name, follower_count, description, country i.e. print(choice_one["country"])
+
+# pull 2 random items from the imported list
+def randomize_choice():
+    return random.choice(data)
+
+def find_answer(choice_a, choice_b):
+    if choice_a["follower_count"] > choice_b["follower_count"]:
+        return "a"
+    else:
+        return "b"
+
+def map_choices(choice_a, choice_b):
+    print(f"Compare A: {choice_a['name']}, a {choice_a['description']}, from {choice_a['country']}.")
+    print(vs)
+    print(f"Against B: {choice_b['name']}, a {choice_b['description']}, from {choice_b['country']}.")
+
+
+print(logo)
+def game(): 
+    score = 0
+    keep_playing = True
+    while keep_playing == True:
+        choice_a = randomize_choice()
+        choice_b = randomize_choice()
+        map_choices(choice_a, choice_b)
+        answer = find_answer(choice_a, choice_b)
+        print(f"Psst! The answer is {answer}!")
+        guess = input("Who has more follower? A or B: ").lower()
+        if guess == answer:
+            score += 1
+            print(f"You're right! Currenct score: {score}.")
+        else:
+            print(f"Sorry, that's wrong. Final score: {score}.")
+            keep_playing = False
+game()
